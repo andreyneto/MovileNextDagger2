7af72e7ed3609fa7d1a3d66d88b4783ff4d4ac5e
Andrey Neto <me@andreyneto.com.br>
2019-02-16 17:25:06 -0200

    Update DIDagger2.kt

--- app/src/main/java/br/com/andreyneto/movilenextdagger2/DIDagger2.kt
+++ app/src/main/java/br/com/andreyneto/movilenextdagger2/DIDagger2.kt
@@ -1,14 +1,36 @@
 package br.com.andreyneto.movilenextdagger2
 
+import dagger.Component
 import dagger.Module
 import dagger.Provides
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
-    @Provides
+    @Provides @Singleton
     fun provideOkHttpClient() = OkHttpClient()
+}
 
-    @Provides
-    fun proviteTwitterApi(client: OkHttpClient) = TwitterApi(client)
+@Module
+class TwitterModule(private val user: String) {
+
+    @Provides @Singleton
+    fun provideTweeter(api: TwitterApi) = Tweeter(api, user)
+
+    @Provides @Singleton
+    fun provideTimeline(api: TwitterApi) = Tweeter(api, user)
+}
+
+class TwitterApplication @Inject constructor
+    (private val tweeter: Tweeter, private val timeline: Timeline)
+
+@Singleton
+@Component(
+    modules = [NetworkModule::class, TwitterModule::class]
+)
+interface TwitterComponent {
+    fun tweeter(): Tweeter
+    fun timeline(): Timeline
 }
